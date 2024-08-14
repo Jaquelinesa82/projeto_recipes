ae637620392e69c5e067b6e20d6da9dac10534b3
jaqueline <jaquelinesa.82@gmail.com>
2024-08-14 12:58:52 -0300

    added static_url and media_url configuration

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 app_name = "recipes"
 
@@ -7,3 +10,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/<int:id>/', views.recipe, name='recipe')
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # for serving media files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # for serving static files
