c436a4e1248c2d0173f9cb73421599689f8cdc5e
jaqueline <jaquelinesa.82@gmail.com>
2024-09-22 10:53:44 -0300

    added list_display to recipe and category admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["title", "is_published", "category", "author"]  
