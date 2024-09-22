dda62f3f2954652a60f999e4b82235b513e700f3
jaqueline <jaquelinesa.82@gmail.com>
2024-09-22 10:57:25 -0300

    Added category url

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 
 app_name = "recipes"
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('recipes/<int:id>/', views.recipe, name='recipe')
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
-
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # for serving media files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # for serving static files
