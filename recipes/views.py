5729115fda4779e4090b11bc7f8ce8bcf5e1fc78
jaqueline <jaquelinesa.82@gmail.com>
2024-09-23 15:55:33 -0300

    added when there are no recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from utils.factory import make_recipe
+from django.shortcuts import render, get_list_or_404, get_object_or_404 # noqa
+# from utils.factory import make_recipe
 from .models import Recipe
 
 
@@ -8,19 +8,29 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes
     })
-    
-    
+
+
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        is_published=True, 
-        category__id=category_id, ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True,
+            category__id=category_id, ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes
-    })  
-    
-     
-def recipe(request):
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | ',
+
+    })
+
+
+def recipe(request, id):
+    recipe = get_object_or_404(
+        Recipe,
+        pk=id,
+        is_published=True
+    )
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
